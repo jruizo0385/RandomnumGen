62bbd0f69d9fef462fd4693c7ca5d33289e6da66
Jose Raul <denver0385@hotmail.com>
2022-04-17 19:40:23 -0600

    first commit

---
+++ src/RandomNunGen/RandomNunGen.java
@@ -0,0 +1,43 @@
+          package RandomNunGen;
+
+          import java.util.Arrays;
+          import java.util.Random;
+
+           public class RandomNunGen {
+
+	       public static void main(String[] args) {
+		   Random rnum = new Random();
+		   int start = 5;
+	       int end = 999;
+	       int counter = 500;
+
+	       int smallest;
+	       int greatest;
+	       int randomInt ;
+
+	       int numbers [] = new int[counter];
+	       Random random = new Random(); 
+
+	       randomInt = random.nextInt(end-start) + start;
+	       numbers [0] = randomInt;
+	       smallest = randomInt;
+	       greatest = randomInt;
+
+	       System.out.println(" Greatest : " + greatest + "\n");
+	       System.out.println(" Smallest : " + smallest + "\n");
+
+	       for(int i=1;i<counter;i++) {
+
+	       numbers[i] = random.nextInt(end-start) + start;
+	       smallest = Math.min(smallest,numbers[i]);
+	       greatest = Math.max(greatest,numbers[i]);
+
+	       System.out.println("Random numbers: " + Arrays.toString(numbers) + "\n\n");
+	       System.out.println(i + " Greatest : " + greatest + "\n");
+	       System.out.println(i + " Smallest : " + smallest + "\n");
+
+	       }
+	  }
+	}
+
+
